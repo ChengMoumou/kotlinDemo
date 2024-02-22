24c415fb3bec4aaf03d5b391687eed8ddfe4bad9
Cara <956144678@qq.com>
2024-02-22 10:30:13 +0800

    暂存-2

--- app/src/main/java/com/example/kotlinpractice1/SimpleWorkwe.kt
+++ app/src/main/java/com/example/kotlinpractice1/SimpleWorkwe.kt
@@ -8,7 +8,6 @@ import androidx.work.WorkerParameters
 class SimpleWorkwe(context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams) {
     override fun doWork(): Result {
-        TODO("Not yet implemented")
         Log.d("SimpleWorker", "do work in SimpleWorker")
         return Result.success()
     }
